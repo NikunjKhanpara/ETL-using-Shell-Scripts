8b346dd9171a7d1d89fe6b862d9a6888dd15fe3d
NikunjKhanpara <114206162+NikunjKhanpara@users.noreply.github.com>
2022-12-30 16:10:46 +0530

    Update cp-access-log.sh

--- cp-access-log.sh
+++ cp-access-log.sh
@@ -1,4 +1,3 @@
-
 # Download the access log file
 
 wget "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Bash%20Scripting/ETL%20using%20shell%20scripting/web-server-access-log.txt.gz"
